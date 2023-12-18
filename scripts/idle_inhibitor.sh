066a05e41d8f9a06d6cdd5ce96156e4581f91ac1
Marlon Kern <marlon@kern.earth>
2023-12-18 12:43:14 +0100

    feat: add scripts and services

---
+++ scripts/idle_inhibitor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SERVICE="swayidle.service"
+
+# Function to check if the service is active
+is_service_active() {
+    systemctl --user is-active --quiet $SERVICE
+}
+
+# Toggle the autolock service
+if is_service_active; then
+    systemctl --user stop $SERVICE
+else
+    systemctl --user start $SERVICE
+fi
---
+++ scripts/idle_status.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SERVICE="swayidle.service"
+
+# Function to check if the service is active
+is_service_active() {
+    systemctl --user is-active --quiet $SERVICE
+}
+
+# Toggle the autolock service
+if is_service_active; then
+    STATE="Idle"
+    MESSAGE="󰾪"
+else
+    STATE="Critical"
+    MESSAGE="󰅶"
+fi
+
+echo "{\"state\":\"$STATE\",\"text\":\"$MESSAGE\"}"
